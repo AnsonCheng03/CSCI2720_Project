3e9cfc32b8302f9960f7be9455647d0a56e46b27
Anson Cheng <Anson.12666@icloud.com>
2024-12-08 04:55:11 +0800

    feat: change data to table view

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,13 +1,50 @@
 "use client";
 
+import { Key } from "react";
 import { useEventContext } from "./context";
 import styles from "./page.module.css";
 
 export default function Home() {
   const { eventData } = useEventContext();
+
+  // venuec	venuee	latitude	longitude	@_id	@_eventCount
+  const eventKeyMap: { [key: string]: string } = {
+    "@_id": "ID",
+    venuee: "Location",
+    "@_eventCount": "Number of Events",
+  };
+  const eventDataArray = Object.values(eventData);
+
   return (
     <div className={styles.page}>
-      <pre>{JSON.stringify(eventData, null, 2)}</pre>
+      {/*turn it as a table  */}
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(eventKeyMap).map((key: string) => (
+              <th key={key}>{eventKeyMap[key]}</th>
+            ))}
+            <th>Add to Favorite</th>
+          </tr>
+        </thead>
+        <tbody>
+          {eventDataArray.map((data) => (
+            <tr key={data.id}>
+              {Object.keys(eventKeyMap).map((key: string) => (
+                <td key={key}>{data[key as keyof typeof data]}</td>
+              ))}
+              <td>
+                <input
+                  type="checkbox"
+                  id={data["@_id"]}
+                  name={data["@_id"]}
+                  value={data["@_id"]}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
