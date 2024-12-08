dc3ec5bcd5b7153360702c3736e51630491a9958
Anson Cheng <Anson.12666@icloud.com>
2024-12-09 04:09:51 +0800

    Feat  search and sort (#12)
    * fix: linter
    
    * chore: add input of filter
    
    * feat: filter GPS distance
    
    * chore: done search and category feature

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Key, useState } from "react";
+import { getDistance } from "geolib";
 import { useEventContext } from "./EventProvider/context";
 import { EventTable } from "./EventProvider/eventDataStruct";
 import styles from "./page.module.css";
@@ -8,6 +9,12 @@ import styles from "./page.module.css";
 export default function Home() {
   const { venueData: rawEventData } = useEventContext();
   const [eventData, setEventData] = useState(rawEventData);
+
+  const [filterSettings, setFilterSettings] = useState({
+    gpsMeter: 0,
+    venue: "",
+    category: "",
+  });
 
   const eventKeyMap: { [key: string]: string } = {
     "@_id": "ID",
@@ -26,11 +33,89 @@ export default function Home() {
   const eventDataArray = Object.values(
     modifiedEventData as Record<string, any>
   );
+
+  const mostAppearWords = eventDataArray
+    .map((event) => event.venuee.split(" "))
+    .flat()
+    .reduce((acc: Record<string, number>, word: string) => {
+      word = word.replace(/[^a-zA-Z]/g, "").toLowerCase();
+      acc[word] = (acc[word] || 0) + 1;
+      return acc;
+    }, {}) as Record<string, number>;
+
   return (
     <div className={styles.page}>
+      {/* filters 1. gps meter - latitude,longitude(slider)  2. venuee(input) 3.Catagory (contain specific words like 2)  */}
+      <h1>Events</h1>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={filterSettings.gpsMeter}
+        onChange={(e) =>
+          setFilterSettings({
+            ...filterSettings,
+            gpsMeter: parseInt(e.target.value),
+          })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Search by venue"
+        value={filterSettings.venue}
+        onChange={(e) =>
+          setFilterSettings({ ...filterSettings, venue: e.target.value })
+        }
+      />
+      <select
+        value={filterSettings.category}
+        onChange={(e) =>
+          setFilterSettings({ ...filterSettings, category: e.target.value })
+        }
+      >
+        <option value="">All</option>
+        {Object.entries(mostAppearWords)
+          .sort((a, b) => b[1] - a[1])
+          .slice(0, 4)
+          .map(
+            (word) =>
+              word[0] && (
+                <option key={word[0]} value={word[0]}>
+                  {word[0]}
+                </option>
+              )
+          )}
+      </select>
+
       <EventTable
         mapTable={eventKeyMap}
-        eventDataArray={eventDataArray}
+        eventDataArray={eventDataArray.filter((event) => {
+          if (filterSettings.gpsMeter) {
+            const gpsMeter = filterSettings.gpsMeter;
+            const eventDistance = getDistance(
+              {
+                latitude: event.latitude,
+                longitude: event.longitude,
+              },
+              {
+                latitude: eventDataArray[0].latitude,
+                longitude: eventDataArray[0].longitude,
+              }
+            );
+            console.log(eventDistance, gpsMeter * 1000);
+            return eventDistance <= gpsMeter * 1000;
+          }
+          if (filterSettings.venue) {
+            return event.venuee
+              .toLowerCase()
+              .includes(filterSettings.venue.toLowerCase());
+          }
+          if (filterSettings.category) {
+            console.log(filterSettings.category);
+            return event.venuee.toLowerCase().includes(filterSettings.category);
+          }
+          return true;
+        })}
         setEventData={setEventData}
       />
     </div>
