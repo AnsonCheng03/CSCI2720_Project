715f4a384d7fedc87ec4a5083d2cbb4a77dfdf23
Anson Cheng <Anson.12666@icloud.com>
2024-12-08 14:09:20 +0800

    chore: combine user, admin panel

--- frontend/src/app/panel/layout.tsx
+++ frontend/src/app/panel/layout.tsx
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authDetails";
 import { redirect } from "next/navigation";
@@ -6,12 +7,16 @@ import NavBar from "@/components/navBar/navBar";
 
 export const userNavItems = [
   { name: "Home", href: "/" },
-  { name: "List of Locations", href: "/user" },
-  { name: "List of Events", href: "/user/events" },
-  { name: "Map", href: "/user/map" },
-  { name: "Favorite", href: "/user/favorite" },
-  { name: "No Idea?", href: "/user/noidea" },
+  { name: "List of Locations", href: "/panel" },
+  { name: "List of Events", href: "/panel/events" },
+  { name: "Map", href: "/panel/map" },
+  { name: "Favorite", href: "/panel/favorite" },
+  { name: "No Idea?", href: "/panel/noidea" },
   { name: "Logout", href: "/api/auth/signout" },
+];
+
+export const adminNavItems = [
+  { name: "Manage Database", href: "/panel/admin" },
 ];
 
 export default async function RootLayout({
@@ -24,7 +29,12 @@ export default async function RootLayout({
   };
   if (!sessionPromise) redirect("/");
   const userRole = sessionPromise.user?.role;
-  if (userRole !== "user") redirect("/admin");
+  if (userRole !== "user" && userRole !== "admin") redirect("/panel");
+
+  const heads = await headers();
+  const pathname = heads.get("x-pathname");
+  if (pathname?.startsWith("/panel/admin") && userRole !== "admin")
+    redirect("/panel");
 
   return (
     <EventProvider data={{}}>
