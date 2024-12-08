b6eb832db4de30787c3f1f366c8f8b141fe386c2
Anson Cheng <Anson.12666@icloud.com>
2024-12-09 01:57:34 +0800

    chore: auto update Venue data on db

--- src/app/panel/EventProvider/downloadEvent.tsx
+++ src/app/panel/EventProvider/downloadEvent.tsx
@@ -6,6 +6,7 @@ import styles from "./page.module.css";
 import { downloadEventData } from "@/app/DatabaseProvider/Mutation/Event";
 import { XMLParser } from "fast-xml-parser";
 import { downloadVenueData } from "./downloadVenueData";
+import { handleVenueData } from "@/app/DatabaseProvider/Mutation/Venue";
 
 export default function DownloadEventPage() {
   const { setEventData, setVenueData } = useEventContext();
@@ -14,8 +15,17 @@ export default function DownloadEventPage() {
     downloadEventData().then(async (rawData) => {
       try {
         const venue = await downloadVenueData(rawData);
+
+        const handleServerSideVenueDataResponse = JSON.parse(
+          await handleVenueData(JSON.parse(venue))
+        );
+
         setEventData(JSON.parse(rawData));
-        setVenueData(JSON.parse(venue));
+
+        setVenueData([
+          ...JSON.parse(venue),
+          ...(handleServerSideVenueDataResponse.downloaded || []),
+        ]);
       } catch (e) {
         setEventData([]);
         setVenueData([]);
