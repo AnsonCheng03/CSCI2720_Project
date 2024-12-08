233568e3503fb7d7b41de7c9efcf4a1b6f61f9b1
Anson Cheng <Anson.12666@icloud.com>
2024-12-08 21:47:45 +0800

    chore: display the location details.

--- src/app/panel/location/[id]/page.tsx
+++ src/app/panel/location/[id]/page.tsx
@@ -15,54 +15,75 @@ export default function Page({ params }: { params: { id: string } }) {
 
   const { venueData } = useEventContext();
 
-  const markerDetails =
-    venueData
-      ?.filter((venue: any) => venue["@_id"] == params.id)
-      ?.map((venue: any) => ({
-        title: venue.venuee,
-        url: `https://www.google.com/maps/search/?api=1&query=${venue.latitude},${venue.longitude}`,
-        position: { lat: venue.latitude, lng: venue.longitude },
-      })) || [];
+  const selectedVenue = venueData?.filter(
+    (venue: any) => venue["@_id"] == params.id
+  );
 
-  if (markerDetails.length == 0) {
+  if (selectedVenue?.length == 0) {
     return <p>Location Not found</p>;
   }
 
+  const markerDetails =
+    selectedVenue?.map((venue: any) => ({
+      title: venue.venuee,
+      url: `https://www.google.com/maps/search/?api=1&query=${venue.latitude},${venue.longitude}`,
+      position: { lat: venue.latitude, lng: venue.longitude },
+    })) || [];
+
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
-      <Map
-        style={{ width: "100vw", height: "50vh" }}
-        defaultCenter={markerDetails[0]?.position}
-        defaultZoom={10}
-        gestureHandling={"greedy"}
-        disableDefaultUI={true}
-        mapId={"venueMap"}
-      >
-        {markerDetails.map((marker, index) => {
-          const [markerRef, markerObj] = useAdvancedMarkerRef();
+    <>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
+        <Map
+          style={{ width: "100vw", height: "50vh" }}
+          defaultCenter={markerDetails[0]?.position}
+          defaultZoom={10}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          mapId={"venueMap"}
+        >
+          {markerDetails.map((marker, index) => {
+            const [markerRef, markerObj] = useAdvancedMarkerRef();
+            return (
+              <>
+                <AdvancedMarker
+                  key={index}
+                  position={marker.position}
+                  title={marker.title}
+                  clickable
+                  ref={markerRef}
+                />
+                <InfoWindow anchor={markerObj}>
+                  <a
+                    style={{
+                      color: "black",
+                      backgroundColor: "white",
+                    }}
+                  >
+                    {marker.title}
+                  </a>
+                </InfoWindow>
+              </>
+            );
+          })}
+        </Map>
+      </APIProvider>
+      {/* // Location Details */}
+      <div>
+        {selectedVenue?.map((venue: any, index: number) => {
           return (
-            <>
-              <AdvancedMarker
-                key={index}
-                position={marker.position}
-                title={marker.title}
-                clickable
-                ref={markerRef}
-              />
-              <InfoWindow anchor={markerObj}>
-                <a
-                  style={{
-                    color: "black",
-                    backgroundColor: "white",
-                  }}
-                >
-                  {marker.title}
-                </a>
-              </InfoWindow>
-            </>
+            <div key={index}>
+              <h2>{venue.venuee}</h2>
+              {Object.entries(venue).map(([key, value]) => {
+                return (
+                  <div key={key}>
+                    <b>{key}</b>: {value}
+                  </div>
+                );
+              })}
+            </div>
           );
         })}
-      </Map>
-    </APIProvider>
+      </div>
+    </>
   );
 }
