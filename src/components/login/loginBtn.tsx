d33d90589cb894c0443773a992c1bcbdd21443d0
Anson Cheng <Anson.12666@icloud.com>
2024-12-10 03:44:14 +0800

    chore: show error on main page

--- src/components/login/loginBtn.tsx
+++ src/components/login/loginBtn.tsx
@@ -6,10 +6,14 @@ import { Input } from "@mui/material";
 import { useSessionContext } from "./context";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useSearchParams } from "next/navigation";
 
 export function LoginBtn() {
   const sessionPromise = useSessionContext();
   const session = use(sessionPromise);
+
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -28,25 +32,28 @@ export function LoginBtn() {
     );
   }
   return (
-    <div className={styles.page}>
-      <Input
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Input
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        className={styles.button}
-        onClick={() =>
-          signIn("credentials", { username: username, password: password })
-        }
-      >
-        Sign in
-      </button>
+    <div>
+      <div className={styles.page}>
+        <Input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          onClick={() =>
+            signIn("credentials", { username: username, password: password })
+          }
+        >
+          Sign in
+        </button>
+      </div>
+      {error && <div className={styles.error}>Error: {error}</div>}
     </div>
   );
 }
