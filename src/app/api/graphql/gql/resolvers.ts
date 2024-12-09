a338df437442e2d06b8bd8474ceefd6ac082bb63
Anson Cheng <Anson.12666@icloud.com>
2024-12-10 02:21:14 +0800

    chore: update schemas

--- src/app/api/graphql/gql/resolvers.ts
+++ src/app/api/graphql/gql/resolvers.ts
@@ -1,26 +1,37 @@
 import { InferResolvers } from "garph";
 import { YogaInitialContext } from "graphql-yoga";
+import { queryType } from "./schema";
+import { VenueGQL, UserGQL, EventGQL, CommentGQL } from "./schema";
+import { getUsers } from "@/app/DatabaseProvider/Mutation/User";
+import { downloadEventData } from "@/app/DatabaseProvider/Mutation/Event";
+import { downloadVenueData } from "@/app/DatabaseProvider/Mutation/Venue";
+import { getComments } from "@/app/DatabaseProvider/Mutation/Comment";
 
 type Resolvers = InferResolvers<
-  { Query: typeof queryType; Mutation: typeof mutationType },
+  {
+    Query: typeof queryType;
+  },
   { context: YogaInitialContext }
 >;
 
 export const resolvers: Resolvers = {
   Query: {
-    getTodos: async (_, __, ctx) => {
-      // Placeholder function
-      throw new Error("Not implemented");
+    getVenues: async (_, __, ctx) => {
+      const venues = await downloadVenueData();
+      console.log(venues);
+      return JSON.parse(venues);
     },
-  },
-  Mutation: {
-    addTodo: async (_, { title }, ctx) => {
-      // Placeholder function
-      throw new Error("Not implemented");
+    getUsers: async (_, __, ctx) => {
+      const users = await getUsers();
+      return JSON.parse(users);
     },
-    removeTodo: async (_, { id }, ctx) => {
-      // Placeholder function
-      throw new Error("Not implemented");
+    getEvents: async (_, __, ctx) => {
+      const events = await downloadEventData();
+      return JSON.parse(events);
+    },
+    getComments: async (_, __, ctx) => {
+      const comments = await getComments();
+      return JSON.parse(comments);
     },
   },
 };
--- src/app/api/graphql/gql/schema.ts
+++ src/app/api/graphql/gql/schema.ts
@@ -2,28 +2,72 @@ import { GarphSchema } from "garph";
 
 export const g = new GarphSchema();
 
-export const TodoGQL = g.type("Todo", {
-  id: g.int(),
-  title: g.string(),
-  createdAt: g.int(),
+export const VenueGQL = g.type("Venue", {
+  _id: g.string().description("Unique identifier for the venue"),
+  venuec: g.string().description("Venue name in Chinese"),
+  venuee: g.string().description("Venue name in English"),
+  latitude: g.float().description("Latitude of the venue location"),
+  longitude: g.float().description("Longitude of the venue location"),
+  comment: g
+    .string()
+    .list()
+    .optional()
+    .description("Optional comments for the venue"),
+});
+
+export const UserGQL = g.type("User", {
+  userName: g.string().description("Unique username for the user"),
+  // password: g.string().description("Password for the user"),
+  role: g.string().description("Role of the user"),
+  favouriteVenue: g
+    .ref(VenueGQL)
+    .list()
+    .optional()
+    .description("List of favorite venues for the user"),
+  darkMode: g.boolean().optional().description("Preference for dark mode"),
+});
+
+export const EventGQL = g.type("Event", {
+  _id: g.string().description("Unique identifier for the event"),
+  title: g.string().description("Title of the event"),
+  venue: g.ref(VenueGQL).description("Venue associated with the event"),
+  predate: g.string().description("Date of the event"),
+  description: g.string().description("Description of the event"),
+  presenter: g.string().description("Presenter or organizer of the event"),
+  price: g.string().description("Price of the event"),
+  quota: g.int().optional().description("Quota for the number of attendees"),
+  joinedUsers: g
+    .ref(UserGQL)
+    .list()
+    .optional()
+    .description("Users who joined the event"),
+  likedUsers: g
+    .ref(UserGQL)
+    .list()
+    .optional()
+    .description("Users who liked the event"),
+  fromDownload: g
+    .boolean()
+    .optional()
+    .description("Indicates if the event was downloaded"),
+});
+
+export const CommentGQL = g.type("Comment", {
+  userName: g.string().description("Username of the commenter"),
+  content: g.string().description("Content of the comment"),
+  likedBy: g
+    .ref(UserGQL)
+    .list()
+    .optional()
+    .description("Users who liked the comment"),
 });
 
 export const queryType = g.type("Query", {
-  getTodos: g.ref(TodoGQL).list().description("Gets an array of todos"),
-});
-
-export const mutationType = g.type("Mutation", {
-  addTodo: g
-    .ref(TodoGQL)
-    .args({
-      title: g.string(),
-    })
-    .description("Adds a new todo"),
-  removeTodo: g
-    .ref(TodoGQL)
-    .optional()
-    .args({
-      id: g.int(),
-    })
-    .description("Removes an existing todo"),
+  getVenues: g.ref(VenueGQL).list().description("Retrieve a list of venues"),
+  getUsers: g.ref(UserGQL).list().description("Retrieve a list of users"),
+  getEvents: g.ref(EventGQL).list().description("Retrieve a list of events"),
+  getComments: g
+    .ref(CommentGQL)
+    .list()
+    .description("Retrieve a list of comments"),
 });
