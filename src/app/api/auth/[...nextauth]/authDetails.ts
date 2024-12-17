efc88ecd6ad8c8b1ca6d64ee0bb3e322f11eb0f2
Anson Cheng <Anson.12666@icloud.com>
2024-12-17 23:19:06 +0800

    chore: create placeholder event

--- src/app/api/auth/[...nextauth]/authDetails.ts
+++ src/app/api/auth/[...nextauth]/authDetails.ts
@@ -5,6 +5,71 @@ import {
   createUsers,
   userLogin,
 } from "@/app/DatabaseProvider/Mutation/User";
+import { handleVenueData } from "@/app/DatabaseProvider/Mutation/Venue";
+import { downloadVenueData } from "@/app/panel/EventProvider/downloadVenueData";
+import { uploadData } from "@/app/DatabaseProvider/Mutation/Event";
+
+async function initDatabase() {
+  try {
+    const venue = await downloadVenueData();
+
+    const handleServerSideVenueDataResponse = JSON.parse(
+      await handleVenueData(JSON.parse(venue))
+    );
+
+    const outputVenue = handleServerSideVenueDataResponse.error
+      ? JSON.parse(venue)
+      : [
+          ...(handleServerSideVenueDataResponse.updated || []),
+          ...(handleServerSideVenueDataResponse.inserted || []),
+          ...(handleServerSideVenueDataResponse.downloaded || []),
+        ];
+
+    const randomVenues = outputVenue
+      .sort(() => 0.5 - Math.random())
+      .slice(0, Math.floor(Math.random() * 3) + 3)
+      .map((venue: any) => venue["@_id"]);
+
+    const events = randomVenues.map((venueId: string, index: number) => {
+      const range = Array.from(
+        { length: Math.floor(Math.random() * 3) + 3 },
+        (_, i) => i
+      );
+      return range.map((i) => {
+        return {
+          "@_id": `event${index}-${i}`,
+          titlee: `Event ${index}-${i}`,
+          venueid: venueId,
+          predateE: new Date().toISOString(),
+          desce: `Initial event ${index}-${i}`,
+        };
+      });
+    });
+
+    await Promise.all(
+      events.map((event: any) => {
+        return Promise.all(
+          event.map(async (e: any) => {
+            const newEvent = JSON.parse(await uploadData(e));
+            if (newEvent.error) {
+              console.error(newEvent.message);
+              return null;
+            }
+            return newEvent;
+          })
+        );
+      })
+    );
+
+    await createUsers({
+      userName: "admin",
+      password: bcrypt.hashSync("admin", bcrypt.genSaltSync(8)),
+      role: "admin",
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 export const authOptions = {
   session: { strategy: "jwt" },
@@ -24,19 +89,13 @@ export const authOptions = {
           if (adminCount.error) {
             return null;
           }
+          await initDatabase();
+
           if (
             credentials.username === "admin" &&
             credentials.password === "admin" &&
             adminCount === 0
           ) {
-            await createUsers({
-              userName: credentials.username,
-              password: bcrypt.hashSync(
-                credentials.password,
-                bcrypt.genSaltSync(8)
-              ),
-              role: "admin",
-            });
             const user = JSON.parse(await userLogin(credentials));
             if (user.error) {
               return null;
